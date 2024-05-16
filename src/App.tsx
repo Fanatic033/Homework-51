c5db602a4d3d8c78c67f183f374677f9437bd5c5
ILzat <ilzat.sadykov.2006@bk.ru>
2024-05-17 00:16:19 +0600

    fix and Now the numbers don't repeat

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import './components/Circles/Circles'
 import {useState} from "react";
 import Circles from "./components/Circles/Circles";
 const  App = () => {
-    const [numbers, setNumbers] = useState([5, 11, 16, 23, 32]);
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
+        const getNewNumbers  = () =>{
+            const uniqueNumbers = new Set<number>();
+            while (uniqueNumbers.size < 5) {
+                const randomNumber = Math.floor(Math.random() * 32) + 5;
+                uniqueNumbers.add(randomNumber);
+            }
+            return Array.from(uniqueNumbers).sort((a, b) => a - b);
+        };
     const changeNumbers = () => {
-        const newNumbers = numbers.map(() => Math.floor(Math.random() * (36 - 5 + 1)) + 5);
-        const sortedNumbers = newNumbers.sort((a, b) => a - b);
-        setNumbers(sortedNumbers);
+    const newNumbers = getNewNumbers();
+        setNumbers(newNumbers);
     };
   return (
     <>
