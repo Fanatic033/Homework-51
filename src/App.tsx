4810da2f3ba49e3fabc6714d5a76d94725e578bb
ILzat <ilzat.sadykov.2006@bk.ru>
2024-05-16 22:05:27 +0600

    app tsx ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-
 import './App.css'
+import './components/Circles/Circles'
 import {useState} from "react";
-
 const  App = () => {
-    const [number, setNumbers] = useState([5, 11, 16, 23, 32])
+    const [numbers, setNumbers] = useState([5, 11, 16, 23, 32]);
+
+    const changeNumbers = () => {
+        const newNumbers = numbers.map(() => Math.floor(Math.random() * (36 - 5 + 1)) + 5);
+        setNumbers(newNumbers);
+    };
   return (
     <>
-    <button onClick={}>New numbers</button>
+    <button onClick={changeNumbers}>New numbers</button>
   </>
   )
 }
